a563fd0ebe9a223eb50bd2b0e08050f3952ed82a
GuvidSlayer <andgeorge0015@gmail.com>
2024-04-18 20:15:48 +0300

    a

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css'; // Importați stilurile SimpleLightbox
+
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+const fullGallery = galleryItems
+  .map(
+    galleryItem =>
+      `<a class="gallery__item" href="${galleryItem.original}">
+        <img
+          class="gallery__image"
+          src="${galleryItem.preview}"
+          alt="${galleryItem.description}"
+        />
+      </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', fullGallery);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// player.on('play', function () {
+//   console.log('play the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+// const onPlay = throttle(function () {
+//   player.getCurrentTime().then(function (seconds) {
+//     localStorage.setItem('videoplayer-current-time', seconds);
+//   });
+// }, 1000);
+
+// player.on('timeupdate', onPlay);
+
+// const savedTime = localStorage.getItem('videoplayer-current-time');
+// if (savedTime) {
+//   player.setCurrentTime(savedTime).catch(function (error) {
+//     console.error('Error setting current time:', error);
+//   });
+// }
