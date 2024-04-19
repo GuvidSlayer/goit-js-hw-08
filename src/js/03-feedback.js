767597eb37671276aad890ddc9a1dc409f898649
GuvidSlayer <andgeorge0015@gmail.com>
2024-04-19 18:19:11 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+document.addEventListener('DOMContentLoaded', event => {
+  const form = document.querySelector('.feedback-form');
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
+  const FFS_KEY = 'feedback-form-state';
+
+  const backState = localStorage.getItem(FFS_KEY);
+  if (backState) {
+    const { email, message } = JSON.parse(backState);
+    emailInput.value = email;
+    messageInput.value = message;
+  }
+
+  const saveState = throttle(() => {
+    const state = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem(FFS_KEY, JSON.stringify(state));
+  }, 500);
+
+  form.addEventListener('input', saveState);
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    if (emailInput.value === '' || messageInput.value === '') {
+      return alert('Please fill in all the fields!');
+    }
+
+    const state = {
+      email: emailInput.value.trim(),
+      message: messageInput.value.trim(),
+    };
+    console.log(state);
+    emailInput.value = '';
+    messageInput.value = '';
+    localStorage.removeItem(FFS_KEY);
+  });
+});
